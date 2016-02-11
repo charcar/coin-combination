9ead49bac79ea4461bfb9d99bedf936af7c26985
Charles Carter and Chris Young <ctopheryoung@gmail.com>
2016-02-11 11:20:38 -0800

    wrote tests passed tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,39 +7,64 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
 
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
-        }
-
-        setPort(port);
-
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
-        //RESTful ARCHITECTURE
-        //Use POST to create something on the server
-        //Use GET to retrieve something from the server
-        //Use PUT to change or update something on the server
-        //Use DELETE to remove or delete something on the server
-        //Keep URLs intuitive
-        //Each request from client contains all info necessary for that request
 
-        //ROUTES: Home Page
+        }
+    public static String coinCounter(Integer cents) {
+      Integer quarters = 0;
+      Integer dimes = 0;
+      Integer nickels = 0;
 
-        // get("/", (request, response) -> {
-        //     HashMap<String, Object> model = new HashMap<String, Object>();
+      while ( cents >= 25 ) {
+        cents = cents - 25;
+        quarters++;
+      }
+      while ( cents >= 10) {
+        cents = cents - 10;
+        dimes++;
+      }
+      while ( cents >= 5 ) {
+        cents = cents - 5;
+        nickels++;
+      }
 
-        //     model.put("template", "templates/index.vtl");
-        //     return new ModelAndView(model, layout);
-        // }, new VelocityTemplateEngine());
+      Integer pennies = cents;
 
-        //ROUTES: Identification of Resources
+      String quarterName = "";
+      String dimeName = "";
+      String nickelName = "";
+      String pennyName = "";
 
-        //ROUTES: Changing Resources
+      if (quarters > 1) {
+        quarterName = " quarters, ";
+      } else {
+        quarterName = " quarter, ";
+      }
+
+      if (dimes > 1) {
+        dimeName = " dimes, ";
+      } else {
+        dimeName = " dime, ";
+      }
+
+      if (nickels > 1) {
+        nickelName = " nickels and ";
+      } else {
+        nickelName = " nickel and ";
+      }
+
+      if (pennies > 1) {
+        pennyName = " pennies.";
+      } else {
+        pennyName = " penny.";
+      }
+
+      String coinCount = (quarters + quarterName + dimes + dimeName + nickels + nickelName + pennies + pennyName);
+
+      return coinCount;
+
 
     }
 }
--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -3,9 +3,39 @@ import static org.junit.Assert.*;
 
 public class UnitTest {
 
-  @Rule
-  public DatabaseRule database = new DatabaseRule();
+  @Test
+  public void coinCounter_oneCentEqualsOnePenny_onePenny() {
+    App testApp = new App();
+    assertEquals("0 quarter, 0 dime, 0 nickel and 1 penny.", testApp.coinCounter(1));
+  }
 
-  //Tests go here
+  @Test
+  public void coinCounter_fiveCentsEqualsOneNickel_oneNickel() {
+    App testApp = new App();
+    assertEquals("0 quarter, 0 dime, 1 nickel and 0 penny.", testApp.coinCounter(5));
+  }
 
+  @Test
+  public void coinCounter_eightCentsEqualsOneNickelThreePennies_oneNickelThreePennies() {
+    App testApp = new App();
+    assertEquals("0 quarter, 0 dime, 1 nickel and 3 pennies.", testApp.coinCounter(8));
+  }
+
+  @Test
+  public void coinCounter_seventeenCentsEqualsOneDimeOneNickelTwoPennies_oneDimeOneNickelTwoPennies() {
+    App testApp = new App();
+    assertEquals("0 quarter, 1 dime, 1 nickel and 2 pennies.", testApp.coinCounter(17));
+  }
+
+  @Test
+  public void coinCounter_fortyfourCentsEqualsOneQuarterOneDimeOneNickelFourPennies_oneQuarterOneDimeOneNickelFourPennies() {
+    App testApp = new App();
+    assertEquals("1 quarter, 1 dime, 1 nickel and 4 pennies.", testApp.coinCounter(44));
+  }
+
+  @Test
+  public void coinCounter_sixtynineCentsEqualsTwoQuartersOneDimeOneNickelFourPennies_oneQuarterOneDimeOneNickelFourPennies() {
+    App testApp = new App();
+    assertEquals("2 quarters, 1 dime, 1 nickel and 4 pennies.", testApp.coinCounter(69));
+  }
 }
